d3d82e65297ca5298e840921505ac5009e822dbf
Zhao Li <zhao.li@alumni.usc.edu>
2019-04-07 12:08:02 -1000

    add create view

--- greetings/views.py
+++ greetings/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.urls import reverse_lazy
 from .models import Greeting
 
 class IndexView(generic.ListView):
@@ -8,3 +9,9 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         """Return the all greetings."""
         return Greeting.objects.all()
+
+class CreateView(generic.edit.CreateView):
+    template_name = 'greetings/create.html'
+    model = Greeting
+    fields = ['message']
+    success_url = reverse_lazy('greetings:index') # more robust than hardcoding to /greetings/; directs user to index view after creating a greeting
