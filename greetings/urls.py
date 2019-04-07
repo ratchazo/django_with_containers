9f55daa8a71507ea494a2cb780e58ab2ba5eb1e7
Zhao Li <zhao.li@alumni.usc.edu>
2019-04-07 12:05:00 -1000

    add route for create action

--- greetings/urls.py
+++ greetings/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'greetings' # allows using 'greetings:index' for url and reverse_lazy methods
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('new', views.CreateView.as_view(), name='create'),
 ]
